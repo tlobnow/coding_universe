30f9ec0229444544a65b417160f4991b189267d5
Finn <tlobnow@googlemail.com>
2023-10-30 16:38:31 +0100

    improved script flow

--- scripts/extract_af2c_data.R
+++ scripts/extract_af2c_data.R
@@ -22,8 +22,32 @@ MAIN    = file.path(PATH, LOC, "/")
 # Extract all JSON files from the provided location automatically and store the results in two files (with/without recycles)
 run_extraction(LOC = LOC, MAIN = MAIN, SUMMARY_FOLDER = SUMMARY_FOLDER)
 
+# Take a look at the generated data frames
 # dataframe_with_recycles = fread(paste0(SUMMARY_FOLDER, LOC, "_summaryWithRecycles.csv"))
 dataframe = fread(paste0(SUMMARY_FOLDER, LOC, ".csv"))
+
+plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER)
+plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER, best_only = T)
+plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER, pattern = "_x", best_only = TRUE)
+
+
+
+##### ADDITIONAL DOCUMENTATION OF THE FUNCTIONS USED ###########################
+
+#' Extracts and Processes AlphaFold Results
+#'
+#' This function extracts data from JSON files, processes it, and appends it to summary CSV files.
+#' It's designed to work specifically with results from AF2C.
+#' @param LOC A string representing the location of the target data folder.
+#' @param MAIN A string representing the main directory where the summaries are stored. Defaults to "~/Desktop/SUMMARIES/LOC/".
+#' @param SUMMARY_FOLDER A string representing the directory where summary files are saved. Defaults to "~/Desktop/SUMMARIES/".
+#' @param ADD_2_EXISTING_DF A logical value indicating whether to append the extracted data to an existing dataframe. Defaults to FALSE.
+#' @param EXISTING_DF A string representing the path of the existing dataframe to which the data should be appended, if ADD_2_EXISTING_DF is TRUE.
+#' \dontrun{
+#' run_extraction(LOC = "my_location")
+#' run_extraction(LOC = "my_location", ADD_2_EXISTING_DF = TRUE, EXISTING_DF = "~/path_to_existing_df.csv")
+#' }
+#' 
 
 #' Plot AlphaFold Results
 #'
@@ -39,7 +63,3 @@ dataframe = fread(paste0(SUMMARY_FOLDER, LOC, ".csv"))
 #' @examples
 #' plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER)
 #' plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER, pattern = "specific_pattern", best_only = TRUE)
-
-plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER)
-plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER, best_only = T)
-plot_alphafold_results(LOC = LOC, SUMMARY_FOLDER = SUMMARY_FOLDER, pattern = "_x", best_only = TRUE)
